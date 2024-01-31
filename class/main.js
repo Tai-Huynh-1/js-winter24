cde6b6e9aba05b23cc515df89bc1312b54156a0c
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2024-01-31 15:33:12 -0800

    programming language diff

---
+++ class/main.js
@@ -0,0 +1,16 @@
+console.log("Hello World!");
+
+// JS: Dynamically typed language / Java/ TypeScript: Statically typed language
+var myFirstVariable = "hello world";
+console.log(myFirstVariable); // prints the variable
+
+myFirstVariable = 5; // reassigned to a number
+console.log(myFirstVariable);
+
+// Compile: JS: Interpreted Language (runtime) => Translate the code line by line before execution => CPU => Slower runtime / likely faster dev time (garbage collector / auto. memory management)
+
+// Compile: Java: Compiled Language (compile time) => Java Bytecode (JVM) (runtime) => Assembly Language => Binary Code => CPU => Faster than inter. language, but slower than C/C++ (garbage collector / auto. memory management)
+
+// Compile: C/C++: Compiled Language (compile time) => Assembly Language => Binary Code => CPU => Extremely fast runtime / likely slower dev time / manual memory management
+
+// Memory: Disk/SSD (persistent) (bigger/slower) => RAM (non-persistent) (smaller than disk/faster) => CPU (register memory) (non-persistent)
