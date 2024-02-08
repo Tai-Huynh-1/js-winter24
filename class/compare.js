ee3d1af0e6221ffd0b23404814c775ac8b03b8e3
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2024-02-07 16:18:59 -0800

    binary conversion, comparison, copy by value and array

--- class/compare.js
+++ class/compare.js
@@ -30,23 +30,33 @@ let x = 8; // bin: 0000_1000 => 1*2^3 + 0 + 0 + 0 => 8
 // with 4 digits/bits, I can represent the number -8 to 7 (signed)
 // given x digits, -1*2^(x-1) to 1*2^(x-1)-1 (range of a signed number)
 
+// 8 digits/bits (unsigned), 2^8 = 256 diff. numbers => 0-> 255
+// 8 bits (signed), 2^8 = 256 diff., number range: -128 to 127
+
 // 15 => 1111 unsigned
 // 8 => 1000 unsigned
 // 7 (given 8 bits) => 0000_0111
 // 7 => 0111 => 7 signed => 1000 + 0001 => 1001 => -7
-// 6 => 0110 => 1001 +1 => 1010 = -6
+// -8 = -7 - 1 => 1001 - 0001 = 1000
+// 6 => 0110 => 1001 + 0001 => 1010 = -6 | 1001 + 0001 = 1010 (-6) + 0001 = 1011 (-5)
+// 5 => positive 0101 => (flip all digits & add 1) 1010 + 0001 = 1011 (-5)
+// 4 => 0100 => 1011 + 0001 = 1100 | 1011 + 0001 => 1100
+// -8 => -4
+// 1000, 1001, 1010, 1011, 1100, 1101, 1110, 1111, 0000, 0001, 0010, ...
 
 // signed / unsigned / two's complement
 
 // dec => bin => hex
 
 // compare values
-// triple equal sign
-console.log("7" === 7); // false - comparing string to number (diff types)
+// triple equal sign: compares type then compares value
+console.log("7" === 7); // false - comparing string to number (diff type)
 console.log(7 === 7); // true - comparing number type & number value
 
-// double equal sign
+// double equal sign: convert type then compare value
 console.log("7" == 7); // true - type conversion then compare value -> true
 console.log(7 == 7); // true
 
-// memory address (hex)
+// other primitives
+console.log(0 === false); // false: diff type
+console.log(0 == false); // true: type conversion and value comparion
---
+++ class/copyByValue.js
@@ -0,0 +1,35 @@
+let a = 5; // init. new value 5
+let b = a; // assign a to b (1 time copy) // like init. new value
+let c = 5; // init new value
+
+console.log(a === c); // true
+console.log(a === b); // true
+console.log(b === c); // true
+
+a = 10; // reassigning a to 10
+console.log(a === c); // false: 10 === 5
+console.log(a === b); // false: 10 === 5
+console.log(b === c); // true: 5 === 5
+
+// takeaway: copying primitive values is copy-by-value (1 time copy)
+
+// OBJECTS IN JS
+// ====================== ARRAYS ======================
+const arrA = [999, "name", true, [[]], false, undefined, null, () => {}]; // declaring array literal
+console.log(arrA.length); // 8
+arrA.push(1000); // adding to the end of array
+console.log(arrA.length, arrA);
+arrA[0] = 1000; // replacing elements at a certain index position
+console.log(arrA.length, arrA);
+
+// VECTOR ----->
+// PHYSICS
+// ^
+// |
+// |
+// | --------> v = vy + vx
+
+// LINEAR ALG.
+// 2x + 6y + 9z = 15        [2 6 9 15]          order matters => [6 2 9 15] => 6x + 2y + 9z = 15
+// 8x - 12y + 5z = 100      [8 -12 5 100]
+// x + 5y - 6z = 50         [1 5 -6 50]
