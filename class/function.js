3c4805d3726882060ce645d69322078d26edc400
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2024-02-12 16:03:18 -0800

    functions 1

---
+++ class/function.js
@@ -0,0 +1,14 @@
+console.log("program start");
+
+function greet(greeting) {
+	// function parameters are the inputs to a function in the function signature: ex. greeting
+	console.log(greeting);
+}
+
+// to invoke/call/use a function, we have to call function name with ()
+let g = "hello world";
+greet("hello world"); // an "argument" is the actual input being passed into a function when it is being invoked/used
+
+greet("hello again");
+
+console.log("program ending");
