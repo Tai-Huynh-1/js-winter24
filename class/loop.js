bbe7c4c9e4d2b748dc65fab6a40090d2c658104d
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2024-02-14 16:39:49 -0800

    functions and loops

--- class/loop.js
+++ class/loop.js
@@ -29,4 +29,28 @@ function sum(array) {
 	return result;
 }
 
-console.log(sum([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+// console.log(sum([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+function printReverse(array) {
+	for (let i = array.length - 1; i >= 0; i--) {
+		const element = array[i];
+		console.log(element);
+	}
+}
+
+// printReverse([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+function stringBuilder(arrayOfLetters) {
+	let word = "";
+
+	for (let index = 0; index < arrayOfLetters.length; index++) {
+		const letter = arrayOfLetters[index]; // "h", "e", "l"
+		word = word + letter; // "" + "h" => "h" + "e" => "he" + "l" => "hel"
+	}
+
+	return word;
+}
+
+console.log(stringBuilder(["h", "e", "l", "l", "o"])); // "hello"
+
+// strings with methods
